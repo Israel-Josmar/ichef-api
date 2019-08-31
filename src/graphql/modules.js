f05fbae2fabfb234ccbab8cb0ad737f32bea6647
Josmar Dias <josmarnet@gmail.com>
2019-08-31 11:04:53 -0300

    add dish module

--- src/graphql/modules.js
+++ src/graphql/modules.js
@@ -1,10 +1,12 @@
 import * as viewer from './viewer'
 import * as user from './user'
+import * as dish from './dish'
 
 const modules = [
   // scalars,
   viewer,
   user,
+  dish,
 ]
 
 export default modules
